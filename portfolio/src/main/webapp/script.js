f0fe4308ed13482d44a01c73278e9cb9a30e801a
Tadj <zettazap16@gmail.com>
2020-06-18 17:46:45 +0000

    Implement comment system

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,12 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+async function getData() {
+  const response = await fetch("text/html");
+  const data = await response.text();
+  document.getElementById('data-container').innerText = data;
+}
+
+
+
