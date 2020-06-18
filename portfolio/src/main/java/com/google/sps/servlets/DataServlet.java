65a0f84dd9fd6c666742fb7e697a5e78ef06f0f6
Tadj <zettazap16@gmail.com>
2020-06-18 17:46:45 +0000

    Implement comment system

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,14 +19,49 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import com.google.gson.Gson;
+
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
-@WebServlet("/data")
+@WebServlet("text/html")
 public class DataServlet extends HttpServlet {
+
+  ArrayList<String> textBoxMessages= new ArrayList<String>();
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    //response.setContentType("text/html");
+
+    Query query = new Query("Task").addSort("timestamp", SortDirection.DESCENDING);
+    PreparedQuery results = datastore.prepare(query);
+    for (Entity entity : results.asIterable()) {
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+        
+    response.setContentType("application/json;");
+  }
+  }
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Get the input from the form.
+    String userText = request.getParameter(request, "text-input", "");
+    textBoxMessages.add(userText);
+
+    Entity taskEntity = new Entity("Task");
+    taskEntity.setProperty("user-comments", userComment);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    
+    response.sendRedirect("/index.html");
+
+
+    //response.setContentType("text-input");
+    //response.getWriter().println(Arrays.toString(words));
+
+
+    //boolean upperCase = Boolean.parseBoolean(getParameter(request, "upper-case", "false"));
+    //boolean sort = Boolean.parseBoolean(getParameter(request, "sort", "false"));
   }
 }
